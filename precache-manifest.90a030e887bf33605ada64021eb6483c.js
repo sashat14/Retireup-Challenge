050e8b57bd3958684af3c2f669841c19e7f76493
Sasha <taylork85@gmail.com>
2020-04-26 22:03:15 -0500

    Updates

---
+++ precache-manifest.90a030e887bf33605ada64021eb6483c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f211b0c55acb43ae80d6e1a75e7c1f19",
+    "url": "/Retireup-Challenge/index.html"
+  },
+  {
+    "revision": "1f229f437bd3bc890701",
+    "url": "/Retireup-Challenge/static/css/2.bb750723.chunk.css"
+  },
+  {
+    "revision": "795c0913a708bbd61369",
+    "url": "/Retireup-Challenge/static/css/main.9ca7209b.chunk.css"
+  },
+  {
+    "revision": "1f229f437bd3bc890701",
+    "url": "/Retireup-Challenge/static/js/2.5e9b68c3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Retireup-Challenge/static/js/2.5e9b68c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "795c0913a708bbd61369",
+    "url": "/Retireup-Challenge/static/js/main.68cf36a2.chunk.js"
+  },
+  {
+    "revision": "7946611799c5cc81abbb",
+    "url": "/Retireup-Challenge/static/js/runtime-main.e0f3083b.js"
+  }
+]);
